1b0e609748e84abda30dc28966f6250d98501909
devendra1401 <95442975+devendra1401@users.noreply.github.com>
2023-08-02 00:25:16 +0530

    Add files via upload

--- StateOfficerPortal/public/StateLogin.js
+++ StateOfficerPortal/public/StateLogin.js
@@ -1,3 +1,51 @@
-/**
- * 
- */
+function clearNameErrMsg(){
+    document.getElementById('stateUNameErr').innerHTML="";
+}
+
+function clearPassErrMsg(){
+    document.getElementById('statePassErr'),innerHTML="";
+}
+
+// function validateName() {
+//     const password = document.getElementById('stateUName').value;
+
+//     const lower = /[a-z]/;
+//     const upper = /[A-Z ]/;
+//     const number = /[0-9]/;
+//     const special= /[!@#$%^&*()_+[\]{};':"\\|,.<>?]/;
+
+//     const containsLower = lower.test(password);
+//     const containsUpper = upper.test(password);
+//     const containsNumber = number.test(password);
+//     const containsSpecial = special.test(password);
+
+//     if ((containsLower || containsUpper )&& !(containsNumber) && !(containsSpecial)) {
+//       return true;
+//     } 
+//     else {
+//       alert("Enter valid name");
+//       return false;
+//     }
+// }
+
+function validate() {
+    var x=document.getElementById('stateUName').value;
+    var y=document.getElementById('statePass').value;
+    
+    console.log('value of x '+x);
+    console.log('value of y '+y);
+
+    if(x =="") {
+        //alert('Username cannot be blank');	
+        document.getElementById('stateUNameErr').innerHTML="*Username cannot be blank";
+        return false;
+    }		
+    
+    if(y =="") {
+        //alert('Password cannot be blank');	return false
+        document.getElementById("statePassErr").innerHTML="Password cannot be blank";
+        return false;
+    }			
+    return true;
+}
+
